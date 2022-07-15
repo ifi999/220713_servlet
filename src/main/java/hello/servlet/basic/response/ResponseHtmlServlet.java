96ce573fb8b0ab151a1d5cd5097fb5674cd88ea5
dbs342 <geunwookim@idatabank.com>
2022-07-15 16:10:28 +0900

    json

--- src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 @WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html" )
@@ -15,6 +16,17 @@ public class ResponseHtmlServlet extends HttpServlet {
 
   @Override
   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    // Content-Type: text/html;charset=utf-8
+    response.setContentType("text/html");
+    response.setCharacterEncoding("utf-8");
+
+    PrintWriter writer = response.getWriter();
+    writer.println("<html>");
+    writer.println("<body>");
+    writer.println("  <div>hi?</div>");
+    writer.println("</body>");
+    writer.println("</html>");
 
   }
 
